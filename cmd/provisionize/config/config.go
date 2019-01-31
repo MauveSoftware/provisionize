1322164de1d0cd7d48416f8954da54adab7ba7f5
Daniel Czerwonk <daniel@dan-nrw.de>
2019-01-31 16:35:30 +0100

    add config

--- cmd/provisionize/config/config.go
+++ cmd/provisionize/config/config.go
@@ -1,20 +1,46 @@
 package config
 
+import (
+	"io"
+	"io/ioutil"
+
+	yaml "gopkg.in/yaml.v2"
+
+	"github.com/pkg/errors"
+)
+
 // Config represents the configuration
 type Config struct {
-	ListenAddress   string
-	Ovirt           *OvirtConfig
-	GooglecCloudDNS *GoogleCloudDNSConfig
+	ListenAddress   string                `yaml:"listen_address"`
+	Ovirt           *OvirtConfig          `yaml:"ovirt"`
+	GooglecCloudDNS *GoogleCloudDNSConfig `yaml:"gcloud"`
 }
 
 // OvirtConfig represents to oVirt configuration part
 type OvirtConfig struct {
-	URL      string
-	Username string
-	Password string
+	URL          string `yaml:"url"`
+	Username     string `yaml:"username"`
+	Password     string `yaml:"password"`
+	TemplatePath string `yaml:"template_path"`
 }
 
 // GoogleCloudDNSConfig represents to DNS configuration part
 type GoogleCloudDNSConfig struct {
-	ProjectID string
+	ProjectID string `yaml:"project_id"`
+}
+
+// Load reads a reader and parses the content
+func Load(r io.Reader) (*Config, error) {
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not read from reader")
+	}
+
+	config := &Config{}
+	err = yaml.Unmarshal(b, config)
+	if err != nil {
+		return nil, errors.Wrap(err, "could parse config")
+	}
+
+	return config, nil
 }
--- cmd/provisionize/config/config_test.go
+++ cmd/provisionize/config/config_test.go
@@ -1,1 +1,39 @@
 package config
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoad(t *testing.T) {
+	config := `listen_address: "[::]:1337"
+ovirt:
+  url: https://my-ovirt.instance
+  username: provisionize
+  password: allTheThings
+  template_path: /etc/provisionize/template
+gcloud:
+  project_id: "123456"`
+	expected := &Config{
+		ListenAddress: "[::]:1337",
+		Ovirt: &OvirtConfig{
+			Username:     "provisionize",
+			Password:     "allTheThings",
+			TemplatePath: "/etc/provisionize/template",
+			URL:          "https://my-ovirt.instance",
+		},
+		GooglecCloudDNS: &GoogleCloudDNSConfig{
+			ProjectID: "123456",
+		},
+	}
+
+	r := strings.NewReader(config)
+	cfg, err := Load(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, expected, cfg)
+}
